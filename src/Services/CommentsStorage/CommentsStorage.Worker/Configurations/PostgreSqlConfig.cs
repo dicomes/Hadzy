6df7295fd9b6c11ab61e406ef9c7c21175c9a388
dicomes <manole.mihai@hotmail.com>
2023-08-25 11:59:58 +0300

    Added PostgreSql service to docker compose

---
+++ src/Services/CommentsStorage/CommentsStorage.Worker/Configurations/PostgreSqlConfig.cs
@@ -0,0 +1,8 @@
+namespace CommentsStorage.Worker.Configurations;
+
+public class PostgreSqlConfig
+{
+    public string User { get; set; }
+    public string Password { get; set; }
+    public string DatabaseName { get; set; }
+}
